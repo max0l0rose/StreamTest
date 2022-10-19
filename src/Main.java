6365c9795776fd682ad3b512aee1a66d82299f38
Maxl <max0l0rose@gmail.com>
2022-10-19 14:46:07 +0200

    qweqweqw

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-//	    int[][] arr1 = {{1,2},{3,4}};
-//	    int[] arr12 = Arrays.stream(arr).map(q -> q+10).toArray();
-	    int[][] arr2 = {{1,2},{3,4}};
+	    Integer[][] arr1 = {{1,2},{3,4}};
+	    int[] arr12 = Arrays.stream(arr1).flatMap(q -> Arrays.stream(q).map(w->w+1)).mapToInt(q->q).toArray();
+
+		int[][] arr2 = {{1,2},{3,4}};
 	    int[] arr22 = Arrays.stream(arr2).flatMapToInt(q -> Arrays.stream(new int[] {q[0],q[1],30,40}))
 			                                                .toArray(); //size -> new Integer[size]
 		int a = 1;
